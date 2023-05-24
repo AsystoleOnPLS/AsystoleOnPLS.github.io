262f5b1ade65f61fb49d1b095c6515f46b14b40d
AsystoleOnPLS <134468979+AsystoleOnPLS@users.noreply.github.com>
2023-05-24 15:56:01 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
-$(document).ready(function(){
-    $("a").on('click', function(event) {
+$(document).ready(function() {
+    $(".navbar .nav-link").on('click', function(event) {
         if (this.hash !== "") {
             event.preventDefault();
             var hash = this.hash;
             var navHeight = $(".navbar").outerHeight();
             $('html, body').animate({
                 scrollTop: $(hash).offset().top - navHeight
-            }, 800, function(){
-                // window.location.hash = hash;
+            }, 800, function() {
                 history.pushState(null, null, hash);
             });
         }
